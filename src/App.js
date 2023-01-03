58230f818bfa412767d4c0f53bbe1c7b35f3400e
MASEOKJAE <maasj7514@gmail.com>
2023-01-03 18:06:51 +0900

    2,router

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import './App.css';
 import Day from './component/Day';
 import DayList from './component/DayList';
 import Header from "./component/Header";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <DayList />
-      <Day />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header/>                   {/*Header는 모두가 공통되게 표시*/}
+        <Switch>                    {/*URL 따라 다르게 표시*/}
+          <Route exact path="/">    {/*exact를 꼭 써줘야 됨*/}
+            <DayList />
+          </Route>
+          <Route path="/day">
+            <Day />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
